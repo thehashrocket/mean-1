4954080144b14859a49de9c527b77bc54944bb59
Amos Haviv <mail@amoshaviv.com>
2014-03-26 01:30:58 +0200

    merge conflict

--- public/modules/users/services/users.js
+++ public/modules/users/services/users.js
@@ -1,12 +1,14 @@
 'use strict';
 
-//Users service used for articles REST endpoint
-angular.module('mean.users').factory('Users', ['$resource', function($resource) {
-    return $resource('users/:userId', {
-        userId: '@_id'
-    }, {
-        update: {
-            method: 'PUT'
-        }
-    });
-}]);
+
+//Users service used for users REST endpoint
+angular.module('mean.users').factory('Users', ['$resource',
+	function($resource) {
+		return $resource('users', {}, {
+			update: {
+				method: 'PUT'
+			}
+		});
+	}
+]);
+
