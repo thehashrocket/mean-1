13d6b6ac1dffc2097e79ae01d26c011e9ebd3ff3
Jason Shultz <jasshultz@gmail.com>
2014-03-30 21:14:44 -0600

    adding templates

--- public/modules/users/controllers/users.js
+++ public/modules/users/controllers/users.js
@@ -5,6 +5,21 @@ angular.module('mean.users').controller('UsersController', ['$scope', '$statePar
 
         $scope.authentication = Authentication;
 
+        $scope.test = 'test content';
+
+        $scope.tabs = [
+            { title:'Dynamic Title 1', content: $scope.test },
+            { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        ];
+
+        $scope.alertMe = function() {
+            setTimeout(function() {
+                alert("You've selected the alert tab!");
+            });
+        };
+
+        $scope.navType = 'pills';
+
         $scope.update = function() {
             var user = $scope.user;
             if (!user.updated) {
