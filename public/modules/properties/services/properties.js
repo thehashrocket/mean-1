01b4bb9e02f84d74e7dca79ec73308a318948696
Jason Shultz <jasshultz@gmail.com>
2014-04-01 18:18:38 -0600

    working on properties

--- public/modules/properties/services/properties.js
+++ public/modules/properties/services/properties.js
@@ -1,9 +1,9 @@
 'use strict';
 
-//Articles service used for articles REST endpoint
+//Properties service used for articles REST endpoint
 angular.module('mean.properties').factory('Properties', ['$resource', function($resource) {
     return $resource('properties/:propertyId', {
-        articleId: '@_id'
+        propertyId: '@_id'
     }, {
         update: {
             method: 'PUT'
