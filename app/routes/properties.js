4a2f7bdf7b63e6fb4215f934ecb845676217ac9d
Jason Shultz <jasshultz@gmail.com>
2014-04-06 13:25:23 -0600

    worked on routes, tickets.

--- app/routes/properties.js
+++ app/routes/properties.js
@@ -9,7 +9,11 @@ module.exports = function(app) {
 	app.post('/properties', users.requiresLogin, properties.create);
     app.get('/properties/user/:userID', properties.read);
 	app.get('/properties/:propertyId', properties.read);
-	app.put('/properties/:propertyId', users.requiresLogin, properties.hasAuthorization, properties.update);
+	app.put('/properties/:propertyId', users.requiresLogin, properties.update);
+    app.get('/users/properties', properties.list);
+    app.post('/users/properties', users.requiresLogin, properties.create);
+    app.get('/users/properties/:propertyId', properties.read);
+    app.put('/users/properties/:propertyId', users.requiresLogin, properties.hasAuthorization, properties.update);
     app.put('/properties/tickets/:propertyId', users.requiresLogin, properties.update);
 	app.del('/properties/:propertyId', users.requiresLogin, properties.hasAuthorization, properties.delete);
 
