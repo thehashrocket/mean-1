6fb75d8a1d6ecb53c4cb0c0b0984b3156c2d09c8
Jason Shultz <jasshultz@gmail.com>
2014-04-07 18:36:11 -0600

    code cleanup

--- public/modules/core/controllers/header.js
+++ public/modules/core/controllers/header.js
@@ -11,14 +11,6 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
             uiRoute: '/properties'
         }];
 
-        $scope.findPropertyByUser = function() {
-            Properties.get({
-                userID: Authentication.user._id
-            }, function(property) {
-                $scope.hasProperty = property;
-            });
-        };
-
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
 		};
